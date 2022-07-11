1b631b741a21c2a08175f5dd879595bda5499315
Kang MinHyeok <rkdalsgur032@gmail.com>
2022-07-11 16:09:03 +0900

    feat : [Upbit API] quotation api - getMarketCode component
    - Upbit API에서 quotation api에 해당하는 getMarketCode 컴포넌트 기능
    추가
    - Refresh 버튼을 추가한 전체 마켓코드 조회 예제

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import MarketCode from "./quotation api/getMarcketCode";
+
 function App() {
-  return <h1>React Example for Upbit</h1>;
+  return (
+    <>
+      <h1>React Example for Upbit</h1>
+      <MarketCode />
+    </>
+  );
 }
 
 export default App;
---
+++ src/quotation api/getMarcketCode.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+
+function MarketCode() {
+  // isLoading, marketCodes state 세팅
+  const [isLoading, setIsLoading] = useState(true);
+  const [marketCodes, setMarketCodes] = useState();
+
+  // refresh button
+  const [refresh, setRefresh] = useState(false); // 토글형 state로 사용
+  const handleRefresh = () => {
+    setIsLoading(true);
+    setRefresh((prev) => !prev);
+  };
+
+  // Upbit 상장 코인 fetch 함수
+  const options = { method: "GET", headers: { Accept: "application/json" } };
+  async function fetchMarketCodes() {
+    console.log("fetching Market Codes Started!");
+    const response = await fetch(
+      "https://api.upbit.com/v1/market/all?isDetails=false",
+      options
+    );
+    const result = await response.json();
+    console.log("fetching Market Codes Finished!");
+    setMarketCodes(result);
+    setIsLoading(false);
+  }
+
+  // useEffect에서 fetch 함수 제어
+  useEffect(() => {
+    fetchMarketCodes();
+  }, [refresh]); // refresh update시 fetch marketCode 재실행
+
+  useEffect(() => {
+    console.log(marketCodes);
+  }, [marketCodes]); // marketCodes update시 콘솔에 출력
+
+  return (
+    <>
+      <h3>getMarketCode Example</h3>
+      <button onClick={handleRefresh}>Refresh</button>
+      <h4>Result</h4>
+      {isLoading
+        ? "Loading..."
+        : marketCodes.map((marketCode) => {
+            return (
+              <div
+                key={`${marketCode.market}_${marketCode.english_name}`}
+                id={`${marketCode.market}_${marketCode.english_name}`}
+              >
+                {marketCode.korean_name}
+              </div>
+            );
+          })}
+    </>
+  );
+}
+
+export default MarketCode;
