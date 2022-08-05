42a628ad75cabdb4452f7d1a645c8011adf9026c
Kang MinHyeok <rkdalsgur032@gmail.com>
2022-08-05 19:46:43 +0900

    feat : Google Analytics - React GA4로 적용하기
     On branch master
     Changes to be committed:
    	modified:   .gitignore
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   public/index.html
    	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./Router";
+import ReactGA from "react-ga4";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+/* Google Analytics with React-ga 
+-----------------------------------------------------*/
+const TRACKING_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID || null;
+if (TRACKING_ID) {
+  ReactGA.initialize(TRACKING_ID);
+  ReactGA.send("pageview");
+}
+/*--------------------------------------------------*/
+
 root.render(
   <React.StrictMode>
     <Router />
